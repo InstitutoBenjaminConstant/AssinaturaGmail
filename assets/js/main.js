369b1988f01ab1399a452ac432c0e17099bd8952
Rogerio <sousarogerio84@gmail.com>
2022-10-04 13:34:55 -0300

    Correção do formulário

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,87 +34,50 @@ document.body.removeChild(container)
 }
 
 
-document.querySelector('#copyView').onclick = function () {
-
- // validar();
+  document.querySelector('#copyView').onclick = function () {
+  
+   //validar();
+   ///var geraAssinatura = () =>{
     var nome = document.querySelector('#nome').value;
     var cargo = document.querySelector('#cargo').value;
-    var telefone = document.querySelector('#telefone').value;
-        
-    var html = "<table id='assinatura'><tr><td style='border-right: 1px solid black;'><img style='width: 80px;padding-right: 10px;' src='assets/img/icons/brasao.png'/></td><td><p style='font-family:arial, sans-serif;font-size: 0.9em;margin-left: 5px;    line-height: 1.1em;'><b>"+nome+"</b><br /><span>"+cargo+"</span><br /><span>"+telefone+"</span><br /><a href='http://www.gov.br/ibc' target='_blank' >www.gov.br/ibc</a><br/><span style='display:inline-block;margin-top:3px;'><a href='https://www.youtube.com/channel/UC_Hv0mKFrLdNsqKMnBoT6uQ' target='_blank'><img src='assets/img/icons/youtube-app.png' style='width:20px'/></a><a href='https://www.facebook.com/InstBenjaminConstant'  target='_blank'><img src='assets/img/icons/facebook-app.png' style='width:20px'/></a><a href='https://twitter.com/ibconstant' target='_blank'><img src='assets/img/icons/twitter-app.png' style='width:20px'/></a></span></p></td></tr></table>";
+    var telefone = document.querySelector('#telefone').value; 
+    
+
+    var html = "<table id='assinatura'><tr><td style='border-right: 1px solid black;'><img style='width: 80px;padding-right: 10px;' src='assets/img/icons/brasao.png'/></td><td><p style='font-family:arial, sans-serif;font-size: 0.9em;margin-left: 5px;    line-height: 1.1em;'><b>"+nome+"</b><br /><span>"+cargo+"</span><br /><span>"+telefone+"</span><br /><a href='http://www.gov.br/ibc' target='_blank' >www.gov.br/ibc</a><br/><span style='display:inline-block;margin-top:3px;'><a href='https://www.youtube.com/channel/UC_Hv0mKFrLdNsqKMnBoT6uQ' target='_blank'><img src='assets/img/icons/youtube-app.png' style='width:20px;margin-right:4px'/></a><a href='https://www.facebook.com/InstBenjaminConstant'  target='_blank'><img src='assets/img/icons/facebook-app.png' style='width:20px;margin-right:4px'/></a><a href='https://twitter.com/ibconstant' target='_blank'><img src='assets/img/icons/twitter-app.png' style='width:20px'/></a></span></p></td></tr></table>";
   
-  copyFormatted(html)
-  var assinatura = document.querySelector("#assinatura");
-  assinatura.innerHTML = html;
-}
-
-
-function setError(nome){
-  nome.style.border = '2px solid #e63636';
-  nome.style.display = "block";
-}
-
-function removeError(nome){
-  nome.style.border = '';
-  spans.nome.style.display = 'none';
-}
-
-
-
-
-function validar(){
-  const nome = document.querySelector("#nome");
-  if(nome.value=="" || nome.value.length < 9)
-  {
-    // alert("Preencha o campo NOME corretamente");
-    // document.nome.focus();
-    // return false;
-    setError(nome);
-  }
-  else
-  {
-    removeError(nome);
+    copyFormatted(html)
+    
+    document.querySelector("#assinatura").innerHTML = html;
+    
+   
   }
   
-   
-  var cargo=document.querySelector("#cargo");
-  if(cargo.value=="" || cargo.value.length < 5)
-  {
-    //alert("Preencha o campo CARGO corretamente");
-    //document.cargo.focus();
-    //return false;
-    setError(cargo);
-  }
-  else
-  {
-    removeError(cargo);
-  }
 
-  var telefone= ('(00)0000-0000');
-  var regex= new RegExp ('/^\(\d{2}\)\d{4,5}-\d{4}$/');
-  if(regex.test(telefone)){
-  ! true;
-  }
-  {
-   
-   return false;
 
-  }
-
-}
   
 
 
  
 
-const nomeInput = document.querySelector('#nome');
 
-var validaTexto = (evento) => {
-  if(evento.target.value === "" || evento.target.value.length < 8){
-    evento.target.parentElement.classList.add('error');
+
+const textInputs = document.querySelectorAll('input');
+  
+ var validar = (event) => {      
+  var input = event.target;
+   var re = new RegExp(input.pattern);
+      
+  if(re.test(input.value)){
+    input.parentElement.classList.remove('error');    
   }else{
-    evento.target.parentElement.classList.remove('error');
-  }
+    console.log(input.classList);
+    input.parentElement.classList.add('error');
+  }  
 }
 
-nomeInput.addEventListener('blur', validaTexto());
+for(let i = 0; i < textInputs.length; i++){
+  textInputs[i].addEventListener('blur', validar);
+}
+
+
+  
